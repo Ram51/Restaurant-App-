75b992cac71b0e34b56a99ea68965ec16b1170bc
RAM51 <asheeshyadav92@gmail.com>
2020-04-11 03:00:18 +0530

    initial commit

---
+++ WebAPI/WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private DBModels db = new DBModels();
+
+        // GET: api/Customer
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return db.Customers.Count(e => e.CustomerID == id) > 0;
+        }
+    }
+}
